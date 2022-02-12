45f644e1850ea3f8a979d360ab9979f96feb6dc8
Keith Davies <32884707+keith-davies@users.noreply.github.com>
2022-02-12 01:24:28 -0500

    move test files to .temp folder

--- projects/trainer.host/src/test/java/frc/robot/TestServer.java
+++ projects/trainer.host/src/test/java/frc/robot/TestServer.java
@@ -6,8 +6,8 @@ public class TestServer {
     public static void main(String[] args) {
         Main.main(new String[]{
             "5409",
-            "temp/training/data.csv",
-            "temp/training/model.csv",
+            ".temp/training/data.csv",
+            ".temp/training/model.csv",
             "4",
             "python.exe",
             "python-projects/trainer/main.py"
